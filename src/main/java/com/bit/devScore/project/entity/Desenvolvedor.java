392a815bee28ef13e26766dccd19e5e8f0f264c8
SwarTiff <josesilveira4152@gmail.com>
2023-05-08 20:16:38 -0300

    arrumando cardinalidades

--- src/main/java/com/bit/devScore/project/entity/Desenvolvedor.java
+++ src/main/java/com/bit/devScore/project/entity/Desenvolvedor.java
@@ -22,6 +22,7 @@ public class Desenvolvedor {
     @Column(name = "email", nullable = false, length = 100)
     private String email;
     @Getter
+<<<<<<< HEAD
     @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "id_skill")
     private List<Skill> skills;
@@ -41,5 +42,32 @@ public class Desenvolvedor {
 
     @Column(name = "projeto", nullable = true)
     private Projeto projeto;
+=======
+    @Column(name = "password", nullable = false, length = 30)
+    private String senha;
+
+
+//    @Getter
+//    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+//    @JoinColumn(name = "id_skill")
+//    private Skill skills;
+    @Getter
+    @Column(name = "Profile_picture", nullable = true)
+    private String profile_imagem;
+//    @Getter
+//    @ManyToMany
+//    private Amigo amigo;
+//    @Getter
+//    @OneToMany
+//    @JoinColumn(name = "conquista")
+//    private Conquista conquista;
+//    @Getter
+//    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+//    @Column(name = "projeto", nullable = true)
+//    private Projeto projeto;
+//    @Getter
+//    @ManyToMany
+//    private Comunidade comunidade;
+>>>>>>> 451351a ( Desenvolvedor: controller, DTOS e services)
 
 }
--- src/main/java/com/bit/devScore/project/repositories/Devrepository.java
+++ src/main/java/com/bit/devScore/project/repositories/Devrepository.java
@@ -7,16 +7,33 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+<<<<<<< HEAD
 @Repository
 public interface Devrepository extends JpaRepository <Desenvolvedor, Long> {
 //
+=======
+
+@Repository
+public interface Devrepository extends JpaRepository <Desenvolvedor, Long> {
+
+    @Query("SELECT c FROM Desenvolvedor c WHERE c.email = :email")
+    List<Desenvolvedor> findByEmail(@Param("email") String email);
+
+}
+
+>>>>>>> 451351a ( Desenvolvedor: controller, DTOS e services)
 //    public List<Desenvolvedor> findByNome (final String nome);
 //    @Query ("from Aluno where nome like :nome")
 //    public List<Desenvolvedor> findByNomeLike (@Param ("nome") final String nome);
 //    @Query (value = "select * from alunos where nome like :nome", nativeQuery = true)
 //    public List<Desenvolvedor> findByNomeLikeNative (@Param("nome") final String nome);
 
+<<<<<<< HEAD
 }
 
+=======
+
+
+>>>>>>> 451351a ( Desenvolvedor: controller, DTOS e services)
 
 
