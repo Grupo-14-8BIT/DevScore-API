e946679df74820f3b816517f63dc7b1193dce8c7
WanderfullMystc <wanderfull.mystc@gmail.com>
2023-05-16 20:26:27 -0300

    testando o os rules do git

---
+++ src/main/java/com/bit/devScore/controllers/ProjetoController.java
@@ -0,0 +1,11 @@
+package com.bit.devScore.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/controller")
+public class ProjetoController {
+
+}
